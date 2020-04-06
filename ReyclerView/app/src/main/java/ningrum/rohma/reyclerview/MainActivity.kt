a6b8fb4d1033204a934902e123112f694a406ebf
rohmapuspa <rohmaningrum17@gmail.com>
2020-04-06 16:51:30 +0700

    Praktikum Pemrograman Mobile Pertemuan 8

---
+++ ReyclerView/app/src/main/java/ningrum/rohma/reyclerview/MainActivity.kt
@@ -0,0 +1,37 @@
+package ningrum.rohma.reyclerview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+    val list = ArrayList<Users>()
+    val listUser = arrayOf(
+        "Alpha",
+        "Bravo",
+        "Charlie",
+        "Delta",
+        "Echo",
+        "Foxtrot",
+        "Golf",
+        "Hotel",
+        "India",
+        "Juliet",
+    )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        RecyclerView.setHasFixedSize(true)
+        RecyclerView.layoutManager = LinearLayoutManager(this)
+        for (i in 0 until listUser.size) {
+            list.add(Users(listUser.get(i)))
+            if (listUser.size - 1 == i) {
+                val adapter = Adapter(list)
+                adapter.notifyDataSetChanged()
+                RecyclerView.Adapter = adapter
+            }
+        }
+    }
+}
---
+++ ReyclerView/app/src/main/java/ningrum/rohma/reyclerview/Users.kt
@@ -0,0 +1,4 @@
+package ningrum.rohma.reyclerview
+
+data class Users (val imageResource: Int, val text1: String)
+}
